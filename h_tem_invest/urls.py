247dfaac8c73b6ec23051a39c93b2948ca9cce9b
Divad-043 <84578981+Divad-043@users.noreply.github.com>
2023-01-16 01:36:25 +0100

    Ajout des urls et models

---
+++ h_tem_invest/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+
+urlpatterns = [
+    path('accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+]
